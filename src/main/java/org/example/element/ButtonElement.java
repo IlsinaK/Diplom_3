3cfe472b07cd928d2678eb5cfd19293244f5e32e
Ilsina Komarova <ilsinchik@list.ru>
2025-01-22 00:08:45 +0300

    add element

--- src/main/java/org/example/element/ButtonElement.java
+++ src/main/java/org/example/element/ButtonElement.java
@@ -1,6 +1,7 @@
 package org.example.element;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.enabled;
@@ -11,21 +12,18 @@ public class ButtonElement {
     SelenideElement buttonElement;
 
     public ButtonElement(String locator){
+
         buttonElement = $(new By.ByXPath(locator));
     }
 
+    @Step("Нажать на кнопку")
     public void clickButton(){
         buttonElement.shouldBe(enabled);
         buttonElement.scrollIntoView(true);
         buttonElement.click();
     }
 
-    public void cscrollAndCickButton(){
-        buttonElement.shouldBe(enabled);
-        buttonElement.scrollIntoView(false);
-        buttonElement.click();
-    }
-
+    @Step("Доступна ли кнопка")
     public boolean isEnableButton(){
         return buttonElement.isEnabled();
     }
--- src/main/java/org/example/element/InputElement.java
+++ src/main/java/org/example/element/InputElement.java
@@ -1,6 +1,7 @@
 package org.example.element;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.enabled;
@@ -10,29 +11,19 @@ public class InputElement {
     private SelenideElement inputElement;
 
     public InputElement(String locator) {
+
         inputElement = $(new By.ByXPath(locator));
     }
 
     public InputElement(SelenideElement selenideElement) {
+
         inputElement = selenideElement;
     }
 
-    // Метод для нажатия на элемент и ввода значения
-    public void clickAndSetValue(String inputValue) {
-        inputElement.shouldBe(enabled).click(); // Нажимаем на элемент
-        inputElement.clear(); // Очищаем
-        inputElement.setValue(inputValue); // Устанавливаем значение
-    }
-
+    @Step("Очистить и установить значение")
     public void clearAndSetValue(String inputValue) {
         inputElement.shouldBe(enabled).clear();
         inputElement.setValue(inputValue);
-    }
-
-    public void setValueIfEmpty(String value) {
-        if (inputElement.getValue().isEmpty()) {
-            inputElement.setValue(value);
-        }
     }
 }
 
